dbd9d071bbed8213535edf3a16c03254668ade95
VitorPereiraFontes <vitor.fontes002@gmail.com>
2025-01-18 14:53:12 -0300

    Implementando a comunicação via UART
    Lendo os dados enviados pelo usuário.
    
    Verificando a entrada recebida para disparar as funções relacionadas.
    
    Implementando a funcionalidade de entrada no modo BOOTSEL via software.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 #include "pico/stdlib.h"
+#include "pico/bootrom.h"
 
 int main() {
+    // Define um buffer para armazenar o texto enviado
+    char buffer[1024];
+
     // Inicializa a comunicação UART
     stdio_init_all();
 
     while (true) {
-    }
+        scanf("%s",buffer);
 
-    return 0;
+        // Remove o '\n' final se presente
+        buffer[strcspn(buffer, "\n")] = '\0';  // Substitui '\n' por '\0'
+
+        printf("O comando recebido foi: %s\n",buffer);
+
+        if(strcmp(buffer,"red") == 0){
+
+        }else if(strcmp(buffer,"green") == 0){
+
+        }else if(strcmp(buffer,"blue") == 0){
+
+        }else if(strcmp(buffer,"buzzer") == 0){
+
+        }else if(strcmp(buffer,"boot") == 0){
+            printf("Habilitando o modo bootsel...");
+            reset_usb_boot(0,0);
+        }
+
+        // Limpa o buffer
+        buffer[0] = '\0';
+        sleep_ms(800);
+    }
 }
