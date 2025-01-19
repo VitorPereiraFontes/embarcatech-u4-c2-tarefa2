85171feb655d6da1b5ae0ca1556e85746d8c3fd8
yohannd1 <mitonanan12@gmail.com>
2025-01-19 16:02:34 -0300

    adicionando código de buzzer

--- src/main.c
+++ src/main.c
@@ -3,12 +3,19 @@
 #include "pico/stdlib.h"
 #include "pico/bootrom.h"
 
+#include "buzzer.h"
+
+#define BUZZER_PIN 10
+
 int main() {
     // Define um buffer para armazenar o texto enviado
     char buffer[1024];
 
     // Inicializa a comunicação UART
     stdio_init_all();
+
+	Buzzer bz;
+	Buzzer_init(&bz, BUZZER_PIN);
 
     while (true) {
         scanf("%s",buffer);
@@ -25,7 +32,7 @@ int main() {
         }else if(strcmp(buffer,"blue") == 0){
 
         }else if(strcmp(buffer,"buzzer") == 0){
-
+			Buzzer_play(&bz, 2090, 500);
         }else if(strcmp(buffer,"boot") == 0){
             printf("Habilitando o modo bootsel...");
             reset_usb_boot(0,0);
